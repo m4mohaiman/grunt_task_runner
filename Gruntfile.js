217d68d298e0efb1e0700e1662fd269711c4f1c1
Mohammad A. Mohaiman <m4mohaiman@gmail.com>
2016-05-29 15:14:28 +0600

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt){
+    
+  //configure main project settings
+    grunt.initConfig({
+       
+        //Basic setting and info about using plugins
+        pkg:grunt.file.readJSON('package.json'),
+        
+        //Name of plugIn (plugIn name without 'grunt-contrib-')
+        cssmin:{
+            combine:{
+                files:{
+                    'html/css/main.css':['html/css/style.css','html/css/other.css']
+                }
+            }
+        },
+        
+         //uglify (plugIn name without 'grunt-contrib-uglify')
+        
+            uglify:{
+             dist:{
+                files:{
+                    'html/js/toggle.js.min.js':['html/js/toggle.js']
+                }
+            }
+        },
+        
+        
+    });
+    
+    //Load the plugIn
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    
+    //Do the Task
+    grunt.registerTask('default', ['cssmin','uglify']);
+    //after the task
+    grunt.log.write('Yo Yo baby!! I am Done!!');    
+    
+};
